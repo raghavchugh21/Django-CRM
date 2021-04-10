0706121351d3b55ef8e59c2617636f303ef32655
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-04-10 10:26:07 +0530

    Deployed

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-STATIC_ROOT = "static_root"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'leads.User'
